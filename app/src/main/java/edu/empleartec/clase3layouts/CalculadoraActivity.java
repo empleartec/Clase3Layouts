3373d3ca4f9b70bce1858e7dd8b6107d5764eff8
Gabriel Calero <g@mobaires.com>
2015-11-23 18:29:53 -0300

    Calculadora (solo layout)

---
+++ app/src/main/java/edu/empleartec/clase3layouts/CalculadoraActivity.java
@@ -0,0 +1,14 @@
+package edu.empleartec.clase3layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculadoraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+    }
+
+}
